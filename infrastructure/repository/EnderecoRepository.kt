c2690435eb7a2e612261817528077a99fa4d0166
RAFAEL BISPO GOMES <rafaelgomesgithub@gmail.com>
2025-12-02 20:21:48 -0300

    Delete infrastructure directory

--- infrastructure/repository/EnderecoRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.sistema.trashmap.infrastructure.repository
-
-import com.sistema.trashmap.domain.model.Endereco
-import org.springframework.data.jpa.repository.JpaRepository
-import java.math.BigDecimal
-
-interface EnderecoRepository : JpaRepository<Endereco, Long> {
-    fun findByCoordenadasLatitudeAndCoordenadasLongitude(latitude: Double, longitude: Double): Endereco? //Retorna um endereço segundo a latitude e longitude informadas
-}
